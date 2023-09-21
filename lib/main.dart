efcdeb20a25398189958111e74cad134152a528d
Abhishek kumar <99316496+abhi999999@users.noreply.github.com>
2023-09-21 20:14:30 +0530

    firstCommet

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+void main() {
+  print("object");
+  // Maps: Map represents a collection of key-value pairs.
+
+  Map<int, int> scores = {4: 80, 8: 90};
+
+  // print(scores);
+
+//  print(scores[4]);
+//  print(scores[8]);
+//  print(scores[9]);
+  //print(scores[80]);
+
+  var abhi = {"aaaaaaa", "aaaaa", "aaaaaa"};
+
+  print(abhi);
+
+  //Variables and Data Types
+
+  // var age = 25;
+//var name = "John Doe";
+//  print(name);
+  // print(age);
+
+//  Data Types: Dart has several built-in data types, including:
+
+  // Numbers: int (integers) and double (floating-point numbers).
+
+  int age = 25;
+  print(age);
+  double price = 9.99;
+  print(price);
+
+  // Strings: String represents a sequence of characters.
+
+  String name = "John Doe";
+  print(name);
+
+  // Booleans: bool represents either true or false.
+
+  bool isStudent = false;
+  print(isStudent);
+
+  // Lists: List represents an ordered collection of objects.
+  List<bool> fruits = [true, false, true];
+
+  print(fruits.runtimeType);
+}
